813acb5487b3850a41b6d7d47ba0130809145a94
Greg <gskril@gmail.com>
2021-08-24 17:17:34 -0400

    Add eslint + prettier

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const notifier = require('node-notifier');
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+const notifier = require('node-notifier')
 
 async function getData(url, selector, timeout) {
-	const virtualConsole = new jsdom.VirtualConsole();
-	virtualConsole.sendTo(console, { omitJSDOMErrors: true });
+	const virtualConsole = new jsdom.VirtualConsole()
+	virtualConsole.sendTo(console, { omitJSDOMErrors: true })
 	const dom = await JSDOM.fromURL(url, {
-		runScripts: "dangerously",
-		resources: "usable",
+		runScripts: 'dangerously',
+		resources: 'usable',
 		virtualConsole,
-	});
+	})
 	const data = await new Promise((res, rej) => {
-		const started = Date.now();
+		const started = Date.now()
 		const timer = setInterval(() => {
-			const element = dom.window.document.querySelector(selector);
+			const element = dom.window.document.querySelector(selector)
 			if (element) {
-				res(element.textContent);
-				clearInterval(timer);
+				res(element.textContent)
+				clearInterval(timer)
 			} else if (Date.now() - started > timeout) {
-				rej("Timed out");
-				clearInterval(timer);
+				rej('Timed out')
+				clearInterval(timer)
 			}
-		}, 100);
-	});
-	dom.window.close();
-	return data;
+		}, 100)
+	})
+	dom.window.close()
+	return data
 }
 
-const url = "https://news.ycombinator.com/news";
-const selector = ".athing .title>a:first-of-type";
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const url = 'https://news.ycombinator.com/news'
+const selector = '.athing .title>a:first-of-type'
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const checkForMatchingContent = async () => {
-	const state1 = await getData(url, selector, 2000);
-	await delay(5000);
-	const state2 = await getData(url, selector, 2000);
+	const state1 = await getData(url, selector, 2000)
+	await delay(5000)
+	const state2 = await getData(url, selector, 2000)
 
 	if (state1 === state2) {
-		// console.log('Original content: ' + state1);
-		return checkForMatchingContent();
+		console.log('Original content: ' + state1)
+		return checkForMatchingContent()
 	} else {
 		notifier.notify({
 			title: 'Page Monitor',
-			message: `Top post on HackerNews changed from ${state1} to ${state2}`
+			message: `Top post on HackerNews changed from ${state1} to ${state2}`,
 		})
-		return console.log(`Top post on HackerNews changed from ${state1} to ${state2}`)
+		return console.log(
+			`Top post on HackerNews changed from ${state1} to ${state2}`
+		)
 	}
 }
 
